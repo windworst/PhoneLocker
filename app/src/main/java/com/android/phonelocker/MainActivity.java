b79bf636b2989c00312d2b0c31eadad4b4138f36
xbw <windworst@gmail.com>
2017-10-17 11:30:44 +0800

    add ui

--- app/src/main/java/com/android/phonelocker/MainActivity.java
+++ app/src/main/java/com/android/phonelocker/MainActivity.java
@@ -1,7 +1,14 @@
 package com.android.phonelocker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import phonelocker.PhoneLocker;
 
@@ -9,7 +16,49 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final long expiredTime = System.currentTimeMillis() + 3600 * 1000; //一个小时
+        setContentView(R.layout.main);
+        final int times[] = {5, 10, 15, 30, 45, 60};
+        ViewGroup buttons = (ViewGroup) findViewById(R.id.gridLayout);
+        for (int i = 0, n = buttons.getChildCount(); i < n; ++i) {
+            final int finalI = i;
+            buttons.getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    confirmLock(times[finalI]);
+                }
+            });
+        }
+        Button button = (Button) findViewById(R.id.button);
+        final NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+        numberPicker.setValue(25);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(10000);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmLock(numberPicker.getValue());
+            }
+        });
+    }
+
+    private void confirmLock(final long timeMinute) {
+        if (timeMinute <= 0) {
+            Toast.makeText(this, "时间值错误", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage(String.format("手机将锁定%d分钟", timeMinute))
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        lock(timeMinute * 60);
+                    }
+                }).setNegativeButton("取消", null).show();
+    }
+
+    private void lock(long timeSecond) {
+        final long expiredTime = System.currentTimeMillis() + timeSecond * 1000;
         PhoneLocker.lock(this, getString(R.string.app_name), new PhoneLocker.OnLockCallback() {
             @Override
             public boolean askIfNeedLock() {
